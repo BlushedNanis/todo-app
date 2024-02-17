396e976eb5902dbaf28b8e68897920a858470a60
DanielG <luis_danieltg@outlook.com>
2024-02-17 17:21:40 -0500

    Removed second test and added minutes

--- to_do_list.py
+++ to_do_list.py
@@ -1,9 +1,8 @@
 import functions
 from time import strftime
 
-
-print('This is a git test')
-
+now = strftime("%b %d, %Y - %H:%M:%S")
+print("It is: ", now)
 
 while True:
     user_input = input("Type add, show, edit, complete or exit: ")
